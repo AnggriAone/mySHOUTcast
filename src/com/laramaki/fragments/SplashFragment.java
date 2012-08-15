58cc584b7316378415aa8f8c01c2312e1f71d971
laramaki <leoaramaki@gmail.com>
2012-08-15 14:02:11 -0300

    Added splash fragment and layout.

--- src/com/laramaki/fragments/SplashFragment.java
+++ src/com/laramaki/fragments/SplashFragment.java
@@ -1,19 +1,17 @@
 package com.laramaki.fragments;
 
-import com.laramaki.R;
-
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ActionBarFragment extends Fragment {
+public class SplashFragment extends Fragment {
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.action_bar, null);
-		return view;
+		
+		return super.onCreateView(inflater, container, savedInstanceState);
 	}
 }
