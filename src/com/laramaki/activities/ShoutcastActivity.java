b231e5d6aaf3c700edbf5452312ea146cf6f4c74
laramaki <leoaramaki@gmail.com>
2012-08-15 15:13:37 -0300

    Implemented hide/show of the action bar because of the splash.

--- src/com/laramaki/activities/ShoutcastActivity.java
+++ src/com/laramaki/activities/ShoutcastActivity.java
@@ -6,16 +6,20 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 
 import com.laramaki.R;
+import com.laramaki.fragments.ActionBarFragment;
 import com.laramaki.fragments.SplashFragment;
 import com.laramaki.fragments.StationsFragment;
 import com.laramaki.net.SHOUTcastClient;
 
 public class ShoutcastActivity extends FragmentActivity {
 
+	private ActionBarFragment actionBarFragment;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.shoutcastme);
+		actionBarFragment = new ActionBarFragment();
 	}
 
 	@Override
@@ -30,8 +34,19 @@ public class ShoutcastActivity extends FragmentActivity {
 				.beginTransaction()
 				.replace(R.shoutcastme.main, fragment,
 						fragment.getClass().getCanonicalName()).commit();
+		if (fragment.getTag().equals(StationsFragment.class.getCanonicalName())) {
+			Fragment f = getSupportFragmentManager().findFragmentById(
+					R.shoutcastme.actionBar);
+			getSupportFragmentManager()
+					.beginTransaction()
+					.show(f).commit();
+		} else {
+			Fragment f = getSupportFragmentManager().findFragmentById(
+					R.shoutcastme.actionBar);
+			getSupportFragmentManager().beginTransaction().hide(f).commit();
+		}
 	}
-	
+
 	private void showSplash() {
 		updateBodyWithFragment(new SplashFragment());
 	}
@@ -39,14 +54,14 @@ public class ShoutcastActivity extends FragmentActivity {
 	private void showStations() {
 		updateBodyWithFragment(new StationsFragment());
 	}
-	
+
 	class GetStationsTask extends AsyncTask<Void, Void, Void> {
 		@Override
 		protected Void doInBackground(Void... params) {
 			SHOUTcastClient.getTopRadioStations();
 			return null;
 		}
-		
+
 		@Override
 		protected void onPostExecute(Void result) {
 			showStations();
